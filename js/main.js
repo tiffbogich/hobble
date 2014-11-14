2687e4c76b836ffdd2534884c8e9c3c388b04482
tiffbogich <tiff.bogich@gmail.com>
2014-11-13 22:42:01 -0500

    each function works

--- js/main.js
+++ js/main.js
@@ -1,49 +1,17 @@
-
-//testing json piece, works with data copied here not in .json file
-var data = {
-    "stations": [
-        {
-            "key": "1",
-            "stationName": "14 St",
-            "lat": "40.739885",
-            "lon": "-74.002573",
-            "borough": "Manhattan",
-            "lines": [
-                "A",
-                "C",
-                "E",
-                "L"
-            ],
-            "description": "Elevator at northwest corner of 14th Street and Eighth Avenue",
-            "imageRef": "https://www.google.com/maps/place/8+Av/@40.739765,-74.0025814,3a,90y,10.32h,89.85t/data=!3m4!1e1!3m2!1sty7IPyuYj4DXLtftwMUhjg!2e0!4m7!1m4!3m3!1s0x89c259be8e8588b1:0x7f62d995d48be517!2s14+St!3b1!3m1!1s0x89c259be445ceb49:0xd4fae2a3d56c7a8a!6m1!1e1"
-        },
-        {
-            "key": "2",
-            "stationName": "14 St-Union Square",
-            "lat": "40.734653",
-            "lon": "-73.989946",
-            "borough": "Manhattan",
-            "lines": [
-                "L",
-                "N",
-                "Q",
-                "R"
-            ],
-            "description": "Elevator at northeast corner of 14th Street and Park Avenue South (Union Square East)",
-            "imageRef": "https://www.google.com/maps/@40.7343207,-73.9896751,3a,75y,50.58h,78.23t/data=!3m4!1e1!3m2!1s0iTy4wo0v-mNkxt7l9Ha_Q!2e0!6m1!1e1"
-        }
-      ]
-    };
-
-//this works when the data is pasted locally
-$.each(data.stations, function(entryIndex, entry){
-  alert(this.stationName);
-}); 
-
-
-
-
 $(document).ready(function(){
+
+  $.getJSON('accessible-mta.json')
+    .done(function (data) {
+      $.each(data.stations, function()
+      {
+        console.log(this.stationName);
+      });  
+    })
+    .fail(function (xhr) {
+    alert('I FAILED!')
+    console.log(xhr);
+    });
+
   //close aside window when 'x' is clicked. this works
   $("#circle").click(function(){
     $("aside").hide();
@@ -57,17 +25,6 @@ $(document).ready(function(){
     lat: 40.750799,
     lng: -73.993628
   });
-
-
-  //this works for locally stored json. Need to loop
-  // $.getJSON("accessible-mta.json", function(data){
-  //   var items = [];
-  //   $.each(data.stations, function(entryIndex, entry){
-  //     items.push(entryIndex + entry)''
-  //   });
-  //   alert(items);
-  // });
-  
 
   //add pins for specific stations. This works (not looped yet)
   map.addMarker({
