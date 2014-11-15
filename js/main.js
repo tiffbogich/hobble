4d88a847c5888414d5a348a16dbb5c65b53ce7bb
tiffbogich <tiff.bogich@gmail.com>
2014-11-15 02:50:11 -0500

    complete database and add transfer data data and map markers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  //center basic map on NYC. this works.
+
+  //center basic map on NYC.
   var map;
   map = new GMaps({
     div: '#map',
@@ -7,6 +8,8 @@ $(document).ready(function(){
     lat: 40.750799,
     lng: -73.993628
   });
+
+  //map accessible stations
   $.getJSON('accessible-mta.json')
     .done(function (data){
       $.each(data.stations, function()
@@ -14,20 +17,22 @@ $(document).ready(function(){
         var station = this;
         //add markers to station locations
         map.addMarker({
-          lat: this.lat,
-          lng: this.lon,
-          title: this.stationName,
+          lat: station.lat,
+          lng: station.lon,
+          title: station.stationName,
           // infoWindow:{
           //   content:'<p>'+this.stationName+'</p>'
           // },
+          
           //on click show extra station info below corresponding to the marker
           click: function(){
             $('#instructions').css('text-indent','-9999px');
             $('aside').show(400);
             $('#station-name').html(station.stationName);
             $('#borough').html(station.borough);
-            $('#notes').html(station.notes);
-            $('#lines').html(station.lines).css('letter-spacing','5px');
+            $('#notes').html(station.description);
+            $('#lines').html(station.lines.toString());
+            
             //add panorama images to the aside info
             var panorama;
             panorama = GMaps.createPanorama({
@@ -44,9 +49,48 @@ $(document).ready(function(){
     console.log(xhr);
     });
 
+  //map transfer stations
+  $.getJSON('transfer-mta.json')
+    .done(function (data){
+      $.each(data.transfers, function()
+      {
+        var transfer = this;
+        
+        //add markers to station locations
+        map.addMarker({
+          lat: transfer.lat,
+          lng: transfer.lon,
+          title: transfer.stationName,
+          icon: 'http://www.stad.com/img/whitedot.png',
+          
+          //on click show extra station info below corresponding to the marker
+          click: function(){
+            $('#instructions').css('text-indent','-9999px');
+            $('aside').show(400);
+            $('#station-name').html(transfer.stationName);
+            $('#borough').html(transfer.borough);  
+            $('#notes').html('Transfers Only');
+            $('#lines').html(station.lines.toString());       
+            //add panorama images to the aside info
+            var panorama;
+            panorama = GMaps.createPanorama({
+              el:'#panorama',
+              lat: transfer.lat,
+              lng: transfer.lon
+            });
+          }
+        });
+      });  
+    })
+    .fail(function (xhr) {
+    alert('I FAILED!')
+    console.log(xhr);
+    });
+
   //close aside window when 'x' is clicked. this works
   $('#circle').click(function(){
     $('aside').hide();
   });
+
 });
 
